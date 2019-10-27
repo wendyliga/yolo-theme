aa4d917f68b06e3025193ac8b5d0d54018ede2e2
Wendy Liga <agilydnew@gmail.com>
2019-10-27 07:29:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+DIR="~/Library/Developer/Xcode/UserData/FontAndColorThemes"
+[ ! -d "$DIR" ] && mkdir -p "$DIR"
+
 mv Yolo.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
