55502e0ee42d4c86370144b1802038db3d7d43c3
Wendy Liga <agilydnew@gmail.com>
2020-04-04 23:55:59 +0700

    fix install script

---
+++ install.command
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes
+cp Yolo.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/Yolo.xccolortheme
--- install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="~/Library/Developer/Xcode/UserData/FontAndColorThemes"
-[ ! -d "$DIR" ] && mkdir -p "$DIR"
-
-mv Yolo.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes
